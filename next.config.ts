9230abd44758a327364a5ff61f03c00cbd22f6a7
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-08-31 13:09:16 -0300

    feat: LICENSE

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const repoName = 'meta-tags-generator';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ativa a exportação estática
+  output: 'export',
+  
+  // Define o caminho base para o projeto no GitHub Pages
+  basePath: `/${repoName}`,
+  
+  // Opcional: Desativa o trailingSlash se você prefere URLs sem a barra no final
+  trailingSlash: true,
+  
+  // Opcional: Garante que o componente <Image> funcione na exportação estática
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
